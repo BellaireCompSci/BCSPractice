6798943734c2cf6faf1bd87c99610a2585b51520
Ryan Knightly <reknightly@gmail.com>
2019-04-26 15:53:28 -0500

    Add solution for Unique from 2012 regionals

--- src/uil2012/regionals/part1/Unique.java
+++ src/uil2012/regionals/part1/Unique.java
@@ -6,25 +6,57 @@ import java.util.Scanner;
 
 public class Unique {
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner in = new Scanner(new File("FILENAME.in"));
+		Scanner in = new Scanner(new File("data/uil2012/unique.dat"));
 		int N = in.nextInt();
 		in.nextLine();
 
+		// Generate list of primes
 		ArrayList<Integer> primes = new ArrayList<>();
-		for (int i=2000; i<3000; i++) {
-		    if (isPrime(i)) primes.add(i);
+		primes.add(2);
+		boolean isPrime;
+		for (int i=2; i<=2099; i++) {
+		    isPrime = true;
+		    for (int prime: primes) {
+		        if (i % prime == 0) {
+		            isPrime = false;
+	                break; 
+		        }
+		    }
+		    if (isPrime) primes.add(i);
 		}
 		for(int n=0; n<N; n++) {
-			
+			int num = in.nextInt();
+			System.out.print(num + " ");
+			if (!primes.contains(num)) {
+			    System.out.println("NOT A PRIME YEAR");
+			    continue;
+			}
+			System.out.print("PRIME YEAR");
+			int consecutives = 0;
+			// Check consecutive primes
+			for (int i=0; i<primes.size()-1; i++) {
+			    int sum = primes.get(i) + primes.get(i+1);
+			    int count = 2;
+			    while (sum < num) {
+			        sum += primes.get(i+count++);
+			    }
+			    if (sum == num) {
+			        if (consecutives > 0) {
+			            System.out.println(" AND THE SUM OF MORE THAN ONE SET OF CONSECUTIVE PRIMES");
+			            consecutives = 0;
+			            break;
+			        } else {
+			            consecutives = count;
+			        }
+			    }
+			}
+			if (consecutives > 0) {
+			    System.out.println(" AND THE SUM OF " + consecutives + " CONSECUTIVE PRIMES");
+			} else {
+			    System.out.println();
+			}
 		}
 
 		in.close();
-	}
-	
-	private static boolean isPrime(int n) {
-	    for (int i=2; i<=Math.ceil(Math.sqrt(n)); i++) {
-	        if (n%i == 0) return false;
-	    }
-	    return false;
 	}
 }
